e6e6ba8d3bd6beba98b6a43032c2bd967e9f88e8
Ken Overholt <kroverholt@gmail.com>
2017-09-21 23:22:14 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -62,7 +62,7 @@ int main()
           * another PID controller to control the speed!
           */
           
-          steer_value = 0.5;
+          steer_value = pid.TotalError();
           
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
