924ba910f1538299b5bf1f453aec24cebb3e0206
Ken Overholt <kroverholt@gmail.com>
2017-09-23 19:15:31 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -55,7 +55,7 @@ int main(int argc, char *argv[])
           double speed = std::stod(j[1]["speed"].get<std::string>());
           double angle = std::stod(j[1]["steering_angle"].get<std::string>());
           double steer_value;
-          double throttle = std::stod(j[i]["throttle"].get<std::string>()); //KRO
+          double throttle = std::stod(j[1]["throttle"].get<std::string>()); //KRO
           /*
           * TODO: Calcuate steering value here, remember the steering value is
           * [-1, 1].
